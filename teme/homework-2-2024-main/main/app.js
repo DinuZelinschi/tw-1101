f88ebf5d76b992b1045bc973785f35c1d4933e98
DinuZk <dinuzelinschi@gmail.com>
2024-11-13 16:02:44 +0200

    tema2

--- teme/homework-2-2024-main/main/app.js
+++ teme/homework-2-2024-main/main/app.js
@@ -1,3 +1,8 @@
+/**
+ * the function renders an object to a tagged string and performs token substitution
+ * @param {object} input - a javascript object representing a hierachycal structure  
+ * @param {object} values - a list of key value pairs where the key is a token to be replaced with the value in strings present in input
+ */
 function render(input, values) {
     if (typeof input !== 'object' || typeof values !== 'object') {
         throw new Error('InvalidType');
